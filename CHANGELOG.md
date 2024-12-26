20c39f9ae12e5a0bf767ef0ea76ecf257f3c3d41
SahilAggarwal2004 <aggarwalsahil2004@gmail.com>
2024-12-26 14:11:02 +0530

    [feat]: return processed reactContent immediately instead of initializing with null

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+# 0.1.3 (26-12-2024)
+
+- **feat:** Return processed `reactContent` immediately on the first render instead of initializing with `null` in the `useRemark` hook and `<Remark>` component.
--- src/hooks.tsx
+++ src/hooks.tsx
@@ -1,25 +1,33 @@
-import { ReactElement, useEffect, useState } from "react";
+import { ReactElement, useMemo } from "react";
 import { Fragment, jsx, jsxs } from "react/jsx-runtime";
 import rehypeReact from "rehype-react";
 import remarkParse from "remark-parse";
 import remarkToRehype from "remark-rehype";
 import { unified } from "unified";
 import { UseRemarkOptions } from "./types.js";
+import { assertIsError } from "./utils.js";
 
 export function useRemark({ markdown, rehypePlugins = [], rehypeReactOptions, remarkParseOptions, remarkPlugins = [], remarkToRehypeOptions, onError = console.error }: UseRemarkOptions): ReactElement | null {
-  const [reactContent, setReactContent] = useState<ReactElement | null>(null);
-
-  useEffect(() => {
-    unified()
-      .use(remarkParse, remarkParseOptions)
-      .use(remarkPlugins)
-      .use(remarkToRehype, remarkToRehypeOptions)
-      .use(rehypePlugins)
-      .use(rehypeReact, { ...rehypeReactOptions, Fragment, jsx, jsxs })
-      .process(markdown)
-      .then(({ result }: { result: ReactElement }) => setReactContent(result))
-      .catch(onError);
-  }, [markdown]);
+  const processor = useMemo(
+    () =>
+      unified()
+        .use(remarkParse, remarkParseOptions)
+        .use(remarkPlugins)
+        .use(remarkToRehype, remarkToRehypeOptions)
+        .use(rehypePlugins)
+        .use(rehypeReact, { ...rehypeReactOptions, Fragment, jsx, jsxs }),
+    [rehypePlugins, rehypeReactOptions, remarkPlugins, remarkParseOptions, remarkToRehypeOptions]
+  );
+  const reactContent = useMemo(() => {
+    try {
+      const { result } = processor.processSync(markdown) as { result: ReactElement };
+      return result;
+    } catch (error) {
+      assertIsError(error);
+      onError(error);
+      return null;
+    }
+  }, [markdown, processor, onError]);
 
   return reactContent;
 }
